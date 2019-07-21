3eef071901f90f5e90db279c8c1f9d82d6e6f3e1
GunelHus <guneloh@code.edu.az>
2019-07-21 23:56:13 +0400

    part2 added but not finished

--- JS_Arrays/js/task.js
+++ JS_Arrays/js/task.js
@@ -3,7 +3,7 @@ let students = [];
 let inputs = document.getElementsByClassName("student-inputs");
 let trItems = "";
 
-function Student(name,surname,email,phone,age){
+function Student(name, surname, email, phone, age) {
     this.name = name;
     this.surname = surname;
     this.email = email;
@@ -11,8 +11,33 @@ function Student(name,surname,email,phone,age){
     this.age = age;
 }
 
-function onclickValues(){
-    let student = new Student(inputs[0].value, inputs[1].value,inputs[2].value, inputs[3].value, inputs[4].value);
+function validateInputs(student) {
+    let errorValidationMessage = "";
+    if (student.name == "") {
+        errorValidationMessage += "Student's name is empty\n";
+    }
+    if (student.surname == "") {
+        errorValidationMessage += "Student's surname is empty\n";
+    }
+    if (student.email == "") {
+        errorValidationMessage += "Student's email is empty\n";
+    }
+    if (student.phone == "") {
+        errorValidationMessage += "Student's phone is empty\n";
+    }
+    if (student.age == "" || isNaN (student.age))  {
+        errorValidationMessage += "Student's age is empty or must be number\n";
+    }
+    if(errorValidationMessage != ""){
+        alert(errorValidationMessage);
+        return false;
+    }
+    return true;
+}
+
+function onclickValues() {
+    let student = new Student(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value, inputs[4].value);
+   if( validateInputs(student) === true){
 
     students.push(student);
 
@@ -26,10 +51,11 @@ function onclickValues(){
     inputs[2].value = "";
     inputs[3].value = "";
     inputs[4].value = "";
-
+    }
 }
-function generateLiItems(val, ind, arr ){
+function generateLiItems(val, ind, arr) {
     // liItems+= `<li class = "list-group-item">${val.name} ${val.surname} ${val.email} ${val.phone} -${val.age}</li>`;
+    ind++;
     trItems += `<tr>
     <th scope="row">${ind}</th>
     <td>${val.name}</td>
@@ -39,3 +65,14 @@ function generateLiItems(val, ind, arr ){
     <td>${val.age}</td>
   </tr>`;
 }
+function sortByStudentAge(){
+    students.sort(function(student1, student2){
+        // Kicikden boyuye yash duzmek
+        return student1.age - student2.age;
+    });
+    students.forEach(generateLiItems);
+
+    document.getElementsByClassName("students-list")[0].innerHTML = trItems;
+    trItems = "";
+}
+
